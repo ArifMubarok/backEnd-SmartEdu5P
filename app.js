c63f1fb5508c8c423f4ccba14b29d29c57ee9d90
Arif Rahman M <arifrm28@gmail.com>
2023-10-02 16:49:55 +0700

    fix(email): Fix view and link to reset password and welcome email

--- app.js
+++ app.js
@@ -90,6 +90,7 @@ mongoose
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
   console.log(process.env.NODE_ENV);
+  console.log(path.join(path.resolve(), 'public'));
   console.log(`App running on port ${port}`);
 });
 
